4899293f8f9f1bc1f55db79171fa1f0170fc2b1f
David Restrepo <david.restrepo@sigma3ds.com>
2020-05-19 09:31:20 -0500

    initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,19 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import { header, content, h1, LinkLogo } from "../styles/headerStyle"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+import Navigation from "./nav"
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+const Header = () => {
+  return (
+    <header css={header}>
+      <div css={content}>
+        <LinkLogo to="/">
+          <h1 css={h1}>Hotel Gatsby!</h1>
+        </LinkLogo>
+        <Navigation />
+      </div>
+    </header>
+  )
 }
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,35 @@
 import React from "react"
-import { Link } from "gatsby"
+import Layout from '../components/layout';
+import Banner from '../components/banner';
+import Home from '../components/content/home';
+import UseRooms from '../hooks/use-rooms';
+import RoomPreview from '../components/roomPreview';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { h2Style, UlStyle } from '../styles/roomStyles';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+
+  const rooms = UseRooms();
+
+  return (
+    <Layout>
+      <Banner />
+
+      <Home />
+
+      <br />
+      <h2 css={h2Style}>
+        Nuestras Habitaciones
+      </h2>
+
+      <UlStyle>
+        {rooms.map((room, key) => (
+          <RoomPreview room={room} key={key} />
+        ))}
+      </UlStyle>
+
+    </Layout>
+  );
+}
 
 export default IndexPage
